cbd467e9e751b2c9cde389df8ff4020cea5a740e
dvsn0 <davisohn31@gmail.com>
2024-10-25 13:36:54 -0700

    First commit for timer

---
+++ GCI-APP/GCI_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCI_APPApp.swift
+//  GCI-APP
+//
+//  Created by David on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GCI_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
